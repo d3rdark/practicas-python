302be9793892e3bf0f2ac9e1306fdbf6d2538fca
Ulises Tadeo Perez Garcia <golloquendo@outlook.com>
2025-03-08 20:41:11 -0600

    Eliminamos y agregas nuevas funciones

--- buscarArchivos.py
+++ buscarArchivos.py
@@ -1,8 +1,19 @@
 from pathlib import Path
 
-ruta = Path(r'D:\Música\Post-Punk Rusian\post punk ruso')
-
-# Listamos los elementos
-for elemento in ruta.iterdir():
-    if elemento.is_file():
-        print(f"Archivos: {elemento.name}")
+def crearTxtDeArchivos(ruta):
+    ruta_padre = Path(rf"{ruta}")
+    extensiones_validas = {'.mp3', '.flac'}
+    opcionUsuario = input("Quieres crear un archivo .txt con el\nnombre de los archivos o solo quieres ver los archivos? (S/N): ")
+    
+    match opcionUsuario:
+        case "S":
+            print("Se esta creando el archivo de texto con el nombre de los archivos")
+            for archivos in ruta_padre.glob('*'):
+                if archivos.is_file() and archivos.suffix.lower() in extensiones_validas:
+                    print(archivos.name)
+                
+        case "N":
+            print("No se ha creado el archivo")
+# D:\Música\Post-Punk Rusian\post punk ruso
+ruta_padre = input("Dame la ruta donde quieres ver los archuivos: ")
+crearTxtDeArchivos(ruta_padre)
